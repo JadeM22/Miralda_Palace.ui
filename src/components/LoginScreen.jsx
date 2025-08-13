cbdf77239ab03cd6fdb9149999d51bdabef8fe85
Jade Yuliana Miralda Matute <miraldajade68@gmail.com>
2025-08-13 15:15:06 -0600

    actualizaciones en la ruta de miralda-palace-logo.png

--- src/components/LoginScreen.jsx
+++ src/components/LoginScreen.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom"
 import { useAuth } from "../context/AuthContext";
 import { isValidEmail, validatePassword } from "../utils/validators";
-import logo from '../src/assets/miralda-palace-logo.png';
+import logo from '../components/miralda-palace-logo.png';
 
 const LoginScreen = () => {
 
